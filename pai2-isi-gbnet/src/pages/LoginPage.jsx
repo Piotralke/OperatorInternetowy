63d5105cb20116e2b731759a2b540e1238905e82
Piotr Dziewiecki <piotreq_46@wp.pl>
2023-05-10 12:45:11 +0200

    Panel logowania/rejestracji

---
+++ pai2-isi-gbnet/src/pages/LoginPage.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react"
+
+export default function LoginPage() {
+
+    const [registering, setRegistering] = useState(false);
+    const [isBusiness, setIsBusiness] = useState(false);
+
+    return (
+        <>
+            {registering ? (<div className='flex flex-col w-full min-h-screen bg-gray-300 items-center'>
+                <div className='flex items-center bg-gray-800 w-full p-3'>
+                    <text className="text-white font-bold text-xl">Gb net</text>
+                </div>
+                <div className='flex flex-col items-center justify-center mt-40 border border-black w-1/2 rounded-xl min-h-[50vh] bg-gray-800'>
+                    <text className='font-bold mb-3 text-xl text-blue-500'>
+                        Rejestracja
+                    </text>
+                    <div className="flex flex-row w-full p-4">
+                        <div className="flex flex-col w-1/2 p-4 items-center justify-center">
+                            <div className="flex flex-row w-full space-x-2 m-3">
+                                <input className='p-3 w-1/2 border border-black rounded-xl' type="text" id="password" name="password" placeholder="imię" />
+                                <input className='p-3 w-1/2 border border-black rounded-xl' type="text" id="password" name="password" placeholder="nazwisko" />
+                            </div>
+                            <input className='p-3 m-3 w-full border border-black rounded-xl' type="text" id="login" name="login" placeholder='login' />
+                            <input className='p-3 m-3 w-full border border-black rounded-xl' type="text" id="login" name="login" placeholder='e-mail' />
+                            <input className='p-3 m-3 w-full border border-black rounded-xl' type="password" id="password" name="password" placeholder="haslo" />
+                            <input className='p-3 m-3 w-full border border-black rounded-xl' type="password" id="password" name="password" placeholder="potwierdź haslo" />
+
+                        </div>
+                        <div className="flex flex-col w-1/2 p-4 items-center justify-center">
+                            <input className='p-3 m-3 w-full border border-black rounded-xl' type="text" id="password" name="password" placeholder="pesel" />
+                            <input className='p-3 m-3 w-full border border-black rounded-xl' type="text" id="password" name="password" placeholder="nr telefonu" />
+                            <div className="flex flex-row w-full space-x-2 m-3">
+                                <input className='p-3 border border-black rounded-xl w-3/5' type="text" id="password" name="password" placeholder="ulica" />
+                                <input className='p-3 w-2/5 border border-black rounded-xl' type="text" id="password" name="password" placeholder="nr domu/lokalu" />
+                            </div>
+                            <div className="flex flex-row w-full space-x-2 m-3">
+                                <input className='p-3 border border-black rounded-xl w-2/5' type="text" id="password" name="password" placeholder="kod pocztowy" />
+                                <input className='p-3  w-3/5 border border-black rounded-xl' type="text" id="password" name="password" placeholder="miasto" />
+                            </div>
+                            <div className="w-1/2 flex flex-row items-center space-x-2 p-2">
+                                <input type="checkbox" onChange={() => { setIsBusiness(!isBusiness) }}></input>
+                                <div className="text-white">Klient biznesowy</div>
+
+                            </div>
+                            {isBusiness ? (<input className='p-3 m-3 w-full border border-black rounded-xl' type="text" id="password" name="password" placeholder="nip" />) : null}
+                        </div>
+                    </div>
+
+
+                    <div className="flex flex-row w-1/2 space-x-3 my-3">
+                        <button className=" border border-blue-500 p-6 w-full rounded-xl text-blue-500" onClick={()=>{setRegistering(false)}}>Logowanie</button>
+                        <button className=" border border-blue-500 p-6 w-full text-white bg-blue-500 rounded-xl">Zarejestruj</button>
+                    </div>
+                    <div className="text-white text-xs">TU BEDZIE GOOGLE</div>
+                </div>
+            </div>) : (<div className='flex flex-col w-full min-h-screen bg-gray-300 items-center'>
+                <div className='flex items-center bg-gray-800 w-full p-3'>
+                    <text className="text-white font-bold text-xl">Gb net</text>
+                </div>
+                <div className='flex flex-col items-center justify-center mt-40 border border-black w-1/2 rounded-xl min-h-[50vh] bg-gray-800'>
+                    <text className='font-bold mb-3 text-xl text-blue-500'>
+                        Logowanie
+                    </text>
+                    <input className='p-3 m-3 w-1/2 border border-black rounded-xl' type="text" id="login" name="login" placeholder='login' />
+                    <input className='p-3 m-3 w-1/2 border border-black rounded-xl' type="password" id="password" name="password" placeholder="haslo" />
+                    <div className="flex flex-row w-1/2 space-x-3 my-3">
+                        <button className=" border border-blue-500 p-6 w-full rounded-xl text-blue-500" onClick={() => { setRegistering(true) }}>Rejestracja</button>
+                        <button className=" border border-blue-500 p-6 w-full text-white bg-blue-500 rounded-xl">Zaloguj</button>
+                    </div>
+                    <div className="text-white text-xs">TU BEDZIE GOOGLE</div>
+                </div>
+            </div>)}
+
+        </>
+    )
+}
