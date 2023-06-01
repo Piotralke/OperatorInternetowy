0dfa587a1921098c06ac2118ef7bcac1526b3aee
Piotralke <piotreq_46@wp.pl>
2023-06-01 22:35:00 +0200

    Tego typu
    Co-authored-by: dawid.spychalski00@gmail.com <dawid.spychalski00@gmail.com>

--- pai2-isi-gbnet/src/pages/HomePage.jsx
+++ pai2-isi-gbnet/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Offer from "../components/Offer"
 import AccountData from "../components/AccountData"
 import Balance from "../components/Balance"
+import Messages from "../components/Messages"
 export default function HomePage(){
     return(
-        <div className="flex flex-row flex-wrap justify-center w-full p-4 h-full basis-4/5">
-            <AccountData></AccountData>
+        <div className="flex flex-row flex-wrap justify-center content-center w-full p-4 min-h-full basis-4/5 space-x-4 bg-gray-300">
+            <Messages></Messages>
             <Balance></Balance>
             <AccountData></AccountData>
+            <div className="w-3/4 mt-10 bg-gray-500">CIPECZKI LUBIE</div>
         </div>
     )
 }
