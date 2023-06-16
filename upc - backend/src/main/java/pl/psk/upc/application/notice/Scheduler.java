c09186c197dfa8ae74b493c44ca1da11b00abf7d
Kalghor <kalghor@gmail.com>
2023-06-16 18:36:05 +0200

    Added new fields to userProblemDto

---
+++ upc - backend/src/main/java/pl/psk/upc/application/notice/Scheduler.java
@@ -0,0 +1,8 @@
+package pl.psk.upc.application.notice;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+    private final static String PAYMENT_NOTICE_REMAINDER_MESSAGE = "";
+}
